2dd3a35cf9b423bd8a4b3e5848d272a4d10b6008
hemant-pathak <34942726+hemant-pathak@users.noreply.github.com>
2021-12-07 23:08:50 +0530

    Update Q1.cpp
    done

--- assignment4/Q1.cpp
+++ assignment4/Q1.cpp
@@ -1,1 +1,10 @@
-a
+/*
+ ROOT MPI Process Controls All Communications
+● most straightforward paradigm;
+● maps one MPI process to one SMP node;
+● each MPI process spawns a fixed number of shared memory threads;
+● communication among MPI processes is handled by the main MPI process
+only, at fixed predetermined intervals;
+● allows for tight control of all communications;
+
+*/
